bf25aead5a0af5020d414ece55723bee4434626a
Melvin Gaye <melvind.gaye@gmail.com>
2020-09-17 23:23:24 -0400

    clearifed layout sections

--- Engine/Models/MonsterEncounter.cs
+++ Engine/Models/MonsterEncounter.cs
@@ -18,3 +18,4 @@ namespace Engine.Models
         }
     }
 }
+   
--- WPFUI.Test/WPFUITEST.cs
+++ WPFUI.Test/WPFUITEST.cs
@@ -3,12 +3,13 @@ using Xunit;
 
 namespace WPFUI.Test
 {
+    //Roy Osheroves naming convention - [UnitOfWork_StateUnderTest_ExpectedBehavior] aka MethodName_InputValue_Outputvalue
     public class WPFUITEST
     {
         [Fact]
-        public void Test1()
+        public void Test1() 
         {
-
+            
         }
     }
 }
